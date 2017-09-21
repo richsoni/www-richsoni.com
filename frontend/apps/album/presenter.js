96af6e9e226183791ae620a8f6342ab67e90d3f0
Rich Soni <rich@richsoni.com>
2017-09-20 20:54:18 -0400

    Album Art

--- frontend/apps/album/presenter.js
+++ frontend/apps/album/presenter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from 'moment';
 import HF from "../../shared/header-footer/";
 import {MMDDYYYY} from '../../presenters/';
 import Disqus from "../../shared/disqus/component";
@@ -22,12 +23,13 @@ const Tracklist = (props) => {
 export default class Presenter extends React.Component {
   render() {
     const {release, tracks} = this.props
+    const {slug} = release;
     return <HF>
       <h1>
         <a href="/albums">Albums</a> / {release.title}
       </h1>
-      <h2>Released on {MMDDYYYY(release.realeased_on)}</h2>
-      <div className='albumArt' />
+      <h2 className={styles.releasedOn}>Released on {MMDDYYYY(release.realeased_on)}</h2>
+      <div className={styles.artwork} style={{backgroundImage: `url(/images/releases/${slug}.png)`}} />
       <Tracklist tracks={tracks} />
       <h3>Comments</h3>
       <Disqus />
