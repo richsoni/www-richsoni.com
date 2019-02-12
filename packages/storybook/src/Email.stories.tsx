a347f5a4d0d7ab46a57e355db58762c2f17499ae
Rich Soni <rich@richsoni.com>
2019-02-12 16:19:25 -0500

    add email template to storybook

---
+++ packages/storybook/src/Email.stories.tsx
@@ -0,0 +1,8 @@
+import {TellAStory1} from "@richsoni/emails";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+storiesOf("Emails.TellAStory1", module).add("default", () => (
+  <TellAStory1 text='hi'>
+  hery
+  </TellAStory1>));
