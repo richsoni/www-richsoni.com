eb943ff54ff5ca76d494ef0759b78149f5f16399
Rich Soni <rich@richsoni.com>
2019-03-05 22:02:45 -0500

    fix some build stuff

--- packages/album-art/build/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-declare type Props = {};
-declare class AlbumArt extends React.Component<Props> {
-    static defaultProps: {};
-    render(): JSX.Element;
-}
-export default AlbumArt;
